9f7bd22b599d371b08b2ce10accd4df1fbc28845
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-03-15 23:36:19 +0200

    feat: added Exceptions

--- src/main/java/club/sportsapp/core/exceptions/EntityAlreadyExistsException.java
+++ src/main/java/club/sportsapp/core/exceptions/EntityAlreadyExistsException.java
@@ -1,7 +1,11 @@
 package club.sportsapp.core.exceptions;
 
-public class EntityAlreadyExistsException extends Exception {
-    public EntityAlreadyExistsException(String message) {
-        super(message);
+public class EntityAlreadyExistsException extends AppGenericException {
+
+    private static final String DEFAULT_CODE =  "AlreadyExists";
+
+    public EntityAlreadyExistsException(String code,String message) {
+
+        super(code + DEFAULT_CODE, message);
     }
 }
---
+++ src/main/java/club/sportsapp/core/exceptions/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package club.sportsapp.core.exceptions;
+
+public class EntityNotFoundException extends AppGenericException {
+
+    private static final String DEFAULT_CODE =  "EntityNotFound";
+
+    public EntityNotFoundException(String code,String message) {
+
+        super(code + DEFAULT_CODE, message);
+    }
+}
